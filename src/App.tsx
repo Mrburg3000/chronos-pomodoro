89be985863419c8784bb4ff07c5880d1734e6e76
João Santos <jm0703507@gmail.com>
2025-11-12 17:23:37 -0300

    ms 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { Heading } from './components/Heading';
-
-import './styles/theme.css'
-import './styles/global.css'
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
-  console.log("Oi");
-
   return (
-    <div>
-      <Heading>Olá mundo!</Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        possimus asperiores ipsam, nobis vel praesentium, consequatur provident
-        aut dolores qui distinctio autem dolor impedit laborum. Harum et eaque
-        commodi aperiam.
-      </p>
-    </div>
+    <>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
   );
 }
-
-
