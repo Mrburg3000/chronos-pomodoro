24cf96dff5c745e6bfd7d6e750a606865f03a424
João Santos <jm0703507@gmail.com>
2025-11-13 13:46:45 -0300

    ms 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { Countdown } from "./components/Countdown";
+
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <Countdown />
+      </Container>
+      
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="meuInput">task</label>
+            <input id='meuInput' type="text" />
+          </div>
+          
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
